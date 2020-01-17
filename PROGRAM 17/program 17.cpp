9436cae4a1d94cc78b3be19f13755fdc8b3ec4ec
Himanshu <59916254+Himanshu-180875@users.noreply.github.com>
2020-01-17 13:18:23 +0530

    Add files via upload

---
+++ PROGRAM 17/program 17.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class point
+{
+	private:
+		int a,b;
+		public:
+			void setdata(int x,int y)
+			{
+				a=x;
+				b=y;
+				
+			}
+			void showdata()
+			{
+				cout<<"value of a is "<<a<<"value of b is"<<b;
+			}
+			point operator+(point obj2)
+			{
+				point temp;
+				temp.a=a+obj2.a;
+				temp.b=b+obj2.b;
+				return temp;
+			}
+};
+int main()
+{
+	point obj1,obj2,obj3;
+	obj1.setdata(3,5);
+	obj2.setdata(2,7);
+	obj3=obj1.operator+(obj2);
+	obj3.showdata();
+	
+}
