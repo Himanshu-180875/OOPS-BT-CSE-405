6554a8b6e855c81eb4776c4ff6820775c00ca867
Himanshu <59916254+Himanshu-180875@users.noreply.github.com>
2020-07-02 22:29:16 +0530

    Add files via upload

---
+++ PROGRAM20/program20.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+#include<fstream>
+class File
+{
+  private:
+  string name;
+  string path;
+  int roll_no;
+
+  public:
+  File(string a,int b, string c)
+  {
+	name=a;
+	roll_no=b;
+	path =c;
+  }
+
+   void write()
+   {
+    char data[100];
+    ofstream fout;
+    fout.open(path.c_str(),ios::out); 
+    fout<<"Name: "<<name;
+    fout<<"\nRollno: "<<roll_no;
+    fout.close();
+    
+   }
+   
+   void read(){
+   	char data;
+   	ifstream fin;
+   	fin.open(path.c_str(), ios::in);
+   	while(fin.get(data)){
+   		
+   	cout<<data;
+	   }
+   	
+   	
+   }   	
+};
+
+int main()
+{
+	string path,name;
+	int roll;
+	cout<<"enter the path along with file name:"<<"\n";
+	cin>>path;
+	cout<<path<<"\n";
+	cout<<"enter the name:"<<"\n";
+	cin>>name;
+	cout<<"enter the roll no:"<<"\n";
+	cin>>roll;
+	File obj(name,roll,path);
+	obj.write();
+	cout<<"Reading data from file"<<endl;
+	obj.read();
+	return 0;
+}
